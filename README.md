341618af32d8315e313448532a6615102611179d
Kristopher Green <kristopher_e_green@homedepot.com>
2019-02-21 19:45:20 -0500

    Creating node server foundation with express and basic routes.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # learn-node1
+## Description
+
 First node app used to learn more about node.
+
+## Run the server
+1. Run `npm install`
+
+2. Run `node server.js`
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express'),
+    server = express();
+
+server.set('port', process.env.PORT || 3001);
+
+//Basic routes
+server.get('/', (request, response) => {
+    response.send('Home Page');
+});
+
+server.get('/about', (request, response) => {
+    response.send('About Page')
+});
+
+//Express error handling middleware
+server.use((request, response) => {
+    response.type('text/plain');
+    response.status(505);
+    response.send('Error Page')
+});
+
+//Binding to a port
+server.listen(3000, () => {
+    console.log('Express server started at port 3000');
+});
